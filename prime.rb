8d058bf42bb9e38215a179db8c33d0f20bc4a81c
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 16:39:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  return true if (2..3).include?(num)
+  return false if num < 2
+  x = (num ** 0.5).floor
+  for i in (2..x) do
+    if (num/i.to_f) % 1 == 0
+      return false
+    end
+  end
+  true
+end
