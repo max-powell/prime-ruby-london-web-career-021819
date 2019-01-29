25378bf89e63ff12d21fcae1917c64f4a9114f21
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 16:21:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 def prime?(num)
   factors = 0
   x = (num ** 0.5).floor
-  for i in (2...x) do
-    if num % i == 0
-      return false
+  if num > 1
+    for i in (2...x) do
+      if num % i != 0
+        return true
+      end
     end
   end
-  true
+  false
 end
